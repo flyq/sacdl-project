2a5c7ab4d6de4817e992b75ca91953a32062b278
flyq <flyq951@gmail.com>
2017-03-14 17:36:34 +0800

    add sth

--- nodejs/simpleWebSite/sampleEjs/app.js
+++ nodejs/simpleWebSite/sampleEjs/app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -42,5 +43,12 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+//这里传入了一个密钥加session id
+app.use(cookieParser('Wilson'));
+//使用靠就这个中间件
+app.use(session({ secret: 'wilson'}));
+
+app.listen(8000);
 
 module.exports = app;
